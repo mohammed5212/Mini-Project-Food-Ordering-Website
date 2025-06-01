21b0146782f8ae56b41ebe7319958f5bb15604b0
mohammed5212 <mohammedchpktr@gmail.com>
2025-06-01 21:42:48 +0530

    made changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import ProductList from './components/ProductList'
 
 
 
@@ -9,9 +9,10 @@ function App() {
 
   return (
     <>
-     
+     <ProductList/>
     </>
   )
 }
 
 export default App
+ 
---
+++ src/components/ProductList.jsx
@@ -0,0 +1,20 @@
+import './style/App.css'
+
+const ProductList = () => {
+  return (
+    <>
+      <h1>Products</h1>
+      <div className="productContainer">
+        <div className="productItem">
+          <img
+            src="https://images.pexels.com/photos/674574/pexels-photo-674574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            className="productImage"
+          />
+          <p className="productName">Product Name</p>
+          <p className="productPrize">Prize</p>
+        </div>
+      </div>
+    </>
+  );
+};
+export default ProductList;
