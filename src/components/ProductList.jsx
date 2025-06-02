81f8d53a76e5f301e70695c24fd9ef63d143bbf3
mohammed5212 <mohammedchpktr@gmail.com>
2025-06-02 17:55:01 +0530

    modified product list , index.css and App.css

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,9 +1,31 @@
-import './style/App.css'
+import".src./style/App.css"
 
 const ProductList = () => {
   return (
     <>
       <h1>Products</h1>
+      <div className="productContainer">
+        <div className="productItem">
+          <img
+            src="https://images.pexels.com/photos/674574/pexels-photo-674574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            className="productImage"
+          />
+          <p className="productName">Product Name</p>
+          <p className="productPrice">Price</p>
+        </div>
+      </div>
+        <h1>Products</h1>
+      <div className="productContainer">
+        <div className="productItem">
+          <img
+            src="https://images.pexels.com/photos/674574/pexels-photo-674574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            className="productImage"
+          />
+          <p className="productName">Product Name</p>
+          <p className="productPrice">Price</p>
+        </div>
+      </div>
+        <h1>Products</h1>
       <div className="productContainer">
         <div className="productItem">
           <img
