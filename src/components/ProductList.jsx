c1807a3984d870ad2be7e4fb86a8d22edba44830
mohammed5212 <mohammedchpktr@gmail.com>
2025-06-07 17:35:44 +0530

    modified productlist n product.json

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -10,12 +10,16 @@ const ProductList = () => {
 
       <h1>Products</h1>
       <div className="productContainer">
-        {products.map(product => (
+        {products.length===0 ?(
+          <p className="noProducts">No Products Available.</p>
+
+        ):
+        (products?.map(product => (
           <div className="productItem" key={product.id}>
             <img src={product.image} alt={product.name} className="productImage" />
             <p className="productName">{product.name}</p>
             <p className="productPrice">₹{product.price.toFixed(2)}</p>
-          </div>
+          </div>)
         ))}
       </div>
     </>
