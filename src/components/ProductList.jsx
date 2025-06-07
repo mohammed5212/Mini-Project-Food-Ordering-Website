abb2b269cbf77e6535e289b510300d423c738333
mohammed5212 <mohammedchpktr@gmail.com>
2025-06-07 19:10:20 +0530

    modified PL n A.css

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,8 +1,5 @@
-
-
-
-import"../style/App.css"
-import products from '../data/Products.json';
+import "../style/App.css";
+import products from "../data/Products.json";
 
 const ProductList = () => {
   return (
@@ -12,15 +9,20 @@ const ProductList = () => {
       <div className="productContainer">
         {products.length===0 ?(
           <p className="noProducts">No Products Available.</p>
-
-        ):
-        (products?.map(product => (
-          <div className="productItem" key={product.id}>
-            <img src={product.image} alt={product.name} className="productImage" />
-            <p className="productName">{product.name}</p>
-            <p className="productPrice">₹{product.price.toFixed(2)}</p>
-          </div>)
-        ))}
+        ) : (
+          products?.map((product) => (
+            <div className="productItem" key={product.id}>
+              <img
+                src={product.image}
+                alt={product.name}
+                className="productImage"
+              />
+              <p className="productName">{product.name}</p>
+              <p className="productPrice">₹{product.price.toFixed(2)}</p>
+              <p className="category">{product.category}</p>
+            </div>
+          ))
+        )}
       </div>
     </>
   );
