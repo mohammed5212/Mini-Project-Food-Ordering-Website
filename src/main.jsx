4b70a0f2fa3d9b02ee4e70a492985853c09b6059
mohammed5212 <mohammedchpktr@gmail.com>
2025-06-16 16:20:11 +0530

    modfid main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,15 @@
 
-// import React from 'react';
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import { Provider } from 'react-redux';
-// import store from './store';
-
-// import App from './App.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <Provider store={store}>
-//        <App />
-//        </Provider>
-   
-
-//   </StrictMode>
-// )
-import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom'; // ✅ MUST BE IMPORTED
+import { BrowserRouter } from 'react-router-dom'; 
 import store from './store';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter> {/* ✅ WRAP App HERE */}
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
